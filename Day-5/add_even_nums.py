7784a270f66b4308526036264fa9fe23c77be4b1
anuptripathy04 <89001421+AnupTripathy@users.noreply.github.com>
2022-06-14 21:30:11 +0530

    Commit Changes

--- Day-5/add_even_nums.py
+++ Day-5/add_even_nums.py
@@ -2,7 +2,13 @@
 #
 # i.e. 2 + 4 + 6 + 8 +10 ... + 98 + 100
 #
-# Important, there should only be 1 print statement in your console output. It should just print the final total and not every step of the calculation.
+# important, there should only be 1 print statement in your console output. It should just print the final total and not every step of the calculation.
 #
 # Hint
 # There are quite a few ways of solving this problem, but you will need to use the range() function in any of the solutions.
+
+result = 0
+for i in range(0, 101, 2):
+    result += i
+
+print(result)
