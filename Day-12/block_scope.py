166f58ecfe29391baa5828a87aab142b21cf03ba
anuptripathy04 <89001421+AnupTripathy@users.noreply.github.com>
2022-07-15 14:49:28 +0530

    Commit Changes

---
+++ Day-12/block_scope.py
@@ -0,0 +1,6 @@
+
+if 2 < 5:
+    var001 = 3
+    print(var001)
+
+print(var001)
---
+++ Day-12/modifying_global_var.py
@@ -0,0 +1,8 @@
+var001 = "Hello Programmers"
+
+def a1():
+    global var001
+    var001 += "OOOOO"
+    print(var001)
+
+a1()
