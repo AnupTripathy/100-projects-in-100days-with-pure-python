ef493e12ec7b89a012a92c8ae21032dd6617761c
anuptripathy04 <89001421+AnupTripathy@users.noreply.github.com>
2022-07-23 21:40:42 +0530

    Commit Chanhes

---
+++ Day-17/Objects_constructors/ex.py
@@ -0,0 +1,14 @@
+class User:
+    def __int__(self, username, age, email, password):
+        self.username = username
+        self.age = age
+        self.email = email
+        self.password = password
+
+
+    print("Account successfully created!!!!")
+
+
+
+
+
